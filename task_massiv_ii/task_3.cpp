d6d3887a302baa5672f1b087a0d264d1036d0572
pola0104 <pola0111.0444@gmail.com>
2025-10-07 09:56:12 +0800

    start dz

---
+++ task_massiv_ii/task_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int count = 0;
+    for (int i = 1000; i <= 9999; i++)
+    {
+        int mas[10] = {0};
+        int num = i;
+        bool flag = true;
+        for (int j = 0; j < 4; j++)
+        {
+            int d = num%10;
+            if (mas[d]){
+                flag = false;
+                break;
+            }
+            else mas[d] = 1;
+            num/=10;
+        }
+
+        if (flag)
+        {
+            count ++;
+            //cout<<i<<" ";
+        }
+
+    }
+    cout<<count; //количество
+    return 0;
+}
