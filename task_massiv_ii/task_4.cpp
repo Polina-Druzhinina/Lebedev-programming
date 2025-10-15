e1fbd567ffa9fdd38cae72bd5f6cb8e2af557834
pola0104 <pola0111.0444@gmail.com>
2025-10-15 09:32:42 +0800

    Maximum number of zeros

---
+++ task_massiv_ii/task_4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int n, count_zero = 0, maxi = 0;;
+    cout<<"Enten count number(c): ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the numbers for the array:"<<endl;
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+        if (arr[i]==0){
+            count_zero++;
+        }
+        else{
+            maxi = (maxi<count_zero)?count_zero:maxi;
+            count_zero = 0;
+        }
+        
+    }
+    maxi = (maxi < count_zero) ? count_zero : maxi;
+    cout<<"Maximum number of zeros: "<<maxi<<endl;
+    return 0;
+
+}
